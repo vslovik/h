bd1be6dc59138b61633027e58bbc8ca08c0bba1f
Valeriya Slovikovskaya <vslovik@gmail.com>
2017-11-13 20:11:58 +0100

    initial commit

--- src/main/scala/org/unipi/matrixnorm/MatrixGenerator.scala
+++ src/main/scala/org/unipi/matrixnorm/MatrixGenerator.scala
@@ -3,25 +3,61 @@ package org.unipi.matrixnorm
 import scala.reflect._
 import org.scalacheck._
 
-//object MatrixGenerator {
-//
-//  def matrix[T](g: Gen[T])(order: Int): Gen[Array[Seq[T]]] =
-//    Gen.listOfN(order, Gen.listOfN(order, g)).map(_.toArray)
-//
-//  def main(args: Array[String]): Unit =
-//    matrix(Gen.choose(1,10))(3)
-//    println('a')
-//
-//}
-
 object MatrixGenerator extends App {
 
-  def matrix[T: ClassTag](g: Gen[T])(order: Int): Gen[Array[Array[T]]] =
-    Gen.listOfN(order*order, g).map { squareList =>
-      squareList.toArray.grouped(order).toArray
+//  def matrix[T: ClassTag](g: Gen[T])(order: Int): Gen[Array[Array[T]]] =
+//    Gen.listOfN(order*order, g).map { squareList =>
+//      squareList.toArray.grouped(order).toArray
+//    }
+//
+//  val r = new scala.util.Random
+//  val order:Int = r.nextInt(5)
+//  val limit:Double = r.nextInt(100)
+//
+//  matrix(Gen.choose(0.0, 2.0 + limit))(2 + order).sample.foreach{
+//    rows => rows.foreach{
+//      cols => cols.foreach{
+//        i => print(s"\t$i")
+//      }
+//        println("\n")
+//    }
+//  }
+
+  def matrix[T: ClassTag](g: Gen[T])(rows: Int, cols: Int): Gen[Array[Array[T]]] =
+    Gen.listOfN(rows*cols, g).map { squareList =>
+      squareList.toArray.grouped(cols).toArray
     }
 
-  matrix(Gen.choose(1,10))(3)
-  println('a')
+  val r = new scala.util.Random
+  val rows:Int = r.nextInt(50)
+  val cols:Int = r.nextInt(50)
+  val limit:Double = r.nextInt(100)
+
+  matrix(Gen.choose(0.0, 2.0 + limit))(2 + rows, 2 + cols)
+
+//  matrix(Gen.choose(0.0, 2.0 + limit))(2 + rows, 2 + cols).sample.foreach{
+//    rows => rows.foreach{
+//      cols => cols.foreach{
+//        i => print(s"\t$i")
+//      }
+//        println("\n")
+//    }
+//  }
+
+//  def matrix[ T : ClassTag]( g: Gen[ T ] )( order: Int ): Gen[ Array[ Array[ T ] ] ] =
+//    for {
+//      rowSeq <- Gen.listOfN( order, g )
+//      rowArray = rowSeq.toArray
+//      seqOfRowArrays <- Gen.listOfN( order, rowArray)
+//    } yield seqOfRowArrays.toArray
+//
+//  matrix(Gen.choose(0.0,10.0))(30).sample.foreach{
+//    rows => rows.foreach{
+//      cols => cols.foreach{
+//        i => print(s"\t$i")
+//      }
+//        println("\n")
+//    }
+//  }
 
 }
