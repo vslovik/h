caac08d4d2f312e810bfe37a81bbbc01709417e8
Valeriya Slovikovskaya <valeriya.slovikovskaya@gruppometa.it>
2017-11-15 09:30:04 +0100

    Some examples to learn

---
+++ src/main/scala/org/unipi/matrixnorm/Try1.scala
@@ -0,0 +1,19 @@
+// https://tech.zilverline.com/2011/04/07/serializing-strings-using-scalacheck
+
+package org.unipi.matrixnorm
+
+object Try1 extends App {
+  def serialize(string: String) = string.getBytes
+  def deserialize(bytes: Array[Byte]) = new String(bytes)
+
+  def test(string: String) {
+    val actual = deserialize(serialize(string))
+    println(if (string == actual) "OK ".format(string)
+    else "FAIL expected  got ".format(string, actual))
+  }
+
+  test("")
+  test("foo")
+  test("the quick brown fox jumps over the lazy dog")
+  test("\u2192")
+}
