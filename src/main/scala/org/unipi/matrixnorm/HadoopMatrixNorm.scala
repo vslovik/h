efb7709085f6b88fb7d1aae30aea00acaf54dc5a
Valeriya Slovikovskaya <vslovik@gmail.com>
2017-11-21 22:50:51 +0100

    now lets write mappers

--- src/main/scala/org/unipi/matrixnorm/HadoopMatrixNorm.scala
+++ src/main/scala/org/unipi/matrixnorm/HadoopMatrixNorm.scala
@@ -1,46 +1,70 @@
+// https://www.programcreek.com/java-api-examples/index.php?api=org.apache.hadoop.io.ArrayWritable
 package org.unipi.matrixnorm
 
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
-import org.apache.hadoop.io.IntWritable
-import org.apache.hadoop.io.Text
-import org.apache.hadoop.io.ArrayWritable
-import org.apache.hadoop.io.MapWritable
-import org.apache.hadoop.io.DoubleWritable
+import org.apache.hadoop.io._
 import org.apache.hadoop.mapreduce.Job
 import org.apache.hadoop.mapreduce.Mapper
 import org.apache.hadoop.mapreduce.Reducer
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
+import org.apache.hadoop.io.ArrayWritable
+import org.apache.hadoop.io.ObjectWritable
+
+class Mapper1Key(val matrixIndex: Integer, val rowIndex: Integer)
+class Mapper2Key(val matrixIndex: Integer, val colIndex: Integer, val flag: Boolean)
+class Mapper2Value(val rowIndex: Integer, val colValue: Double)
 
 object HadoopMatrixNorm {
 
-  class MatrixNormMapper extends Mapper[Integer, Text, Text, IntWritable] {
+  class MatrixNormMapper1 extends Mapper[Integer, Text, ObjectWritable, ArrayWritable] {
 
-    override def map(key: Integer, value: Text, context: Mapper[Integer, Text, MapWritable, ArrayWritable]#Context): Unit = {
+    override def map(key: Integer, value: Text, context: Mapper[Integer, Text, ObjectWritable, ArrayWritable]#Context): Unit = {
+
       val matrix = (new MatrixGenerator).deserialize(value.toString)
-      val rows = matrix.length
-      val cols = matrix.head.length
-      var i = 0
-      while (i < matrix.length) {
 
-        val vl = new MapWritable
-        val nn  = new IntWritable(key)
-        val ii =  new IntWritable(i)
-        vl.put(nn, ii)
+//      val rows = matrix.length
+//      val cols = matrix.head.length
 
-        matrix.foreach {
-          row => {
-            val mm = row.map{ e: Double => new DoubleWritable(e)}
-            val v = new ArrayWritable(DoubleWritable.class, mm)
-            context.write(vl, v) //Add Matrix key
-          }
-
-        }
-        i += 1
+      for (index <- matrix.indices) {
+        context.write(
+          new ObjectWritable(new Mapper1Key(key, index)),
+          new ArrayWritable(
+            classOf[DoubleWritable],
+            matrix(index).map { e: Double => new DoubleWritable(e) }.toArray
+          )
+        )
       }
     }
   }
+
+  class MatrixNormMapper2 extends Mapper[ObjectWritable, ArrayWritable, ObjectWritable, ObjectWritable] {
+
+    override def map(key: ObjectWritable, value: ArrayWritable, context: Mapper[ObjectWritable, ArrayWritable, ObjectWritable, ObjectWritable]#Context): Unit = {
+
+      val row = value.get()
+
+      for (i <- row.indices) {
+
+        val k = key.get() match { case j: Mapper1Key => j}
+        val v = row(i) match { case h: DoubleWritable => h }
+
+        context.write(
+          new ObjectWritable(new Mapper2Key(k.matrixIndex, i, false)),
+          new ObjectWritable(new Mapper2Value(k.rowIndex, v.get()))
+        )
+
+        context.write(
+          new ObjectWritable(new Mapper2Key(k.matrixIndex, i, true)),
+          new ObjectWritable(new Mapper2Value(k.rowIndex, v.get()))
+        )
+
+      }
+
+    }
+  }
+
 
   class MatrixNormReducer extends Reducer[Text,IntWritable,Text,IntWritable] {
     override def reduce(key: Text, values: Iterable[IntWritable], context: Reducer[Text, IntWritable, Text, IntWritable]#Context): Unit = {
@@ -65,7 +89,8 @@ object HadoopMatrixNorm {
     val job = Job.getInstance(configuration,"matrix normalization")
     job.setJarByClass(this.getClass)
 
-    job.setMapperClass(classOf[MatrixNormMapper])
+    job.setMapperClass(classOf[MatrixNormMapper1])
+    job.setMapperClass(classOf[MatrixNormMapper2])
     job.setCombinerClass(classOf[MatrixNormCombiner])
     job.setReducerClass(classOf[MatrixNormReducer])
     job.setReducerClass(classOf[MatrixNormComposer])
