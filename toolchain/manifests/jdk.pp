6e3bd859d9b0bf254247a869de192674f8246519
Valeriya Slovikovskaya <vslovik@gmail.com>
2017-12-05 22:25:31 +0100

    provisioning

--- toolchain/manifests/jdk.pp
+++ toolchain/manifests/jdk.pp
@@ -15,9 +15,8 @@
 
 class toolchain::jdk {
   case $::operatingsystem {
-    /Debian/: {
+    /Debian|Ubuntu/: {
       require apt
-      unless $operatingsystemmajrelease > "8" {
          # we pin openjdk-8-* and ca-certificates-java to backports
          require apt::backports
 
@@ -34,26 +33,10 @@ class toolchain::jdk {
          exec {'own_update':
             command => '/usr/bin/apt-get update'
          } -> Package['openjdk-8-jdk']
-      }
 
       package { 'openjdk-8-jdk' :
         ensure => present,
       }
-    }
-    /Ubuntu/: {
-      include apt
-
-      package { 'openjdk-8-jdk' :
-        ensure  => present,
-        # needed for 14.04 
-        require => [ Apt::Ppa[ 'http://ppa.launchpad.net/openjdk-r/ppa/ubuntu'], Class['apt::update'] ]
-      }
-
-      apt::key { 'openjdk-ppa':
-        id     => 'eb9b1d8886f44e2a',
-        server => 'keyserver.ubuntu.com'
-      }  ->
-      apt::ppa { 'http://ppa.launchpad.net/openjdk-r/ppa/ubuntu':  }
     }
     /(CentOS|Amazon|Fedora)/: {
       package { 'java-1.8.0-openjdk-devel' :
--- toolchain/manifests/scala.pp
+++ toolchain/manifests/scala.pp
@@ -1,12 +1,12 @@
 class toolchain::scala {
 
-  $scala_version = '2.11.8'
+  $scala_version = '2.12.4'
   $scala = "scala-${scala_version}"
 
   case $::operatingsystem {
     /Ubuntu|Debian/: {
       exec {'download-scala':
-        command => "/usr/bin/wget http://www.scala-lang.org/files/archive/scala-${scala}.deb",
+        command => "/usr/bin/wget http://www.scala-lang.org/files/archive/${scala}.deb",
         cwd => '/usr/src',
         creates => "/usr/src/${scala}.deb"
       }
@@ -14,7 +14,7 @@ class toolchain::scala {
         ensure => 'latest',
         source   => "/usr/src/${scala}.deb",
         provider => 'dpkg',
-        require => Exec['download-scala']
+        require => [ Exec['download-scala'], Package["openjdk-8-jdk"] ]
       }
     }
     default: {
