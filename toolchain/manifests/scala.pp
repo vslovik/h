742f5bf182f836538a8359960c64dd8a470d0007
Valeriya Slovikovskaya <valeriya.slovikovskaya@gruppometa.it>
2017-12-05 09:08:34 +0100

    Scala provisioning

--- toolchain/manifests/scala.pp
+++ toolchain/manifests/scala.pp
@@ -1,4 +1,5 @@
 # https://github.com/gini/puppet-scala/blob/master/manifests/params.pp
+# https://stackoverflow.com/questions/33953042/cant-provision-scala-sbt-from-same-file-as-jenkins-service
 class toolchain::scala(
   $version        = '2.11.8',
   $url            = 'org.scala-lang',
@@ -22,5 +23,20 @@ class toolchain::scala(
     provider => $package_provider,
     source   => "/var/tmp/scala-${version}.${package_format}",
     require  => Archive::Download["scala-${version}.${package_format}"],
+  }
+
+  ####
+  wget::fetch { "download scala":
+    source => "http://www.scala-lang.org/files/archive/scala-2.10.3.deb",
+    destination => "/tmp/scala-2.10.3.deb",
+    cache_dir => "/vagrant/cache/"
+  }
+
+  package { "libjansi-java": }
+
+  package { "scala":
+    provider => "dpkg",
+    source => "/tmp/scala-2.10.3.deb",
+    require => [ File["/tmp/scala-2.10.3.deb"], Package["openjdk-8-jdk"], Package["libjansi-java"] ]
   }
 }
