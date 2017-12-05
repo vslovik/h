15a74020a7f883777eaeb6486b240e233baf8c44
Valeriya Slovikovskaya <valeriya.slovikovskaya@gruppometa.it>
2017-12-05 08:45:23 +0100

    Scala provisioning

--- toolchain/manifests/scala.pp
+++ toolchain/manifests/scala.pp
@@ -1,29 +1,12 @@
 # https://github.com/gini/puppet-scala/blob/master/manifests/params.pp
 class toolchain::scala(
-  $version        = 'UNSET',
-  $url            = 'UNSET',
-  $package_format = 'UNSET',
+  $version        = '2.11.8',
+  $url            = 'org.scala-lang',
+  $package_format = 'scala-library',
 ) {
 
-  include scala::params
-
-  $version_real = $version ? {
-    'UNSET' => $::scala::params::version,
-    default => $version,
-  }
-
-  $package_format_real = $package_format ? {
-    'UNSET' => $::scala::params::package_format,
-    default => $package_format,
-  }
-
-  $url_real = $url ? {
-    'UNSET' => "http://www.scala-lang.org/files/archive/scala-${version_real}.${package_format_real}",
-    default => $url,
-  }
-
-  archive::download { "scala-${version_real}.${package_format_real}":
-    url        => $url_real,
+  archive::download { "scala-${version}.${package_format}":
+    url        => "http://www.scala-lang.org/files/archive/scala-${version}.${package_format}",
     checksum   => false,
     src_target => '/var/tmp',
   }
@@ -34,10 +17,10 @@ class toolchain::scala(
     default  => fail('Unsupported OS family'),
   }
 
-  package { "scala-${version_real}":
+  package { "scala-${version}":
     ensure   => installed,
     provider => $package_provider,
-    source   => "/var/tmp/scala-${version_real}.${package_format_real}",
-    require  => Archive::Download["scala-${version_real}.${package_format_real}"],
+    source   => "/var/tmp/scala-${version}.${package_format}",
+    require  => Archive::Download["scala-${version}.${package_format}"],
   }
 }
